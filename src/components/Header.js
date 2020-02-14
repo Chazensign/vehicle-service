ff628a4757e7607c590bc8190d38e7e6b1e69c91
Chazensign <chazensign@gmail.com>
2020-02-14 16:22:34 -0700

    header started

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,19 +4,76 @@ import styled from 'styled-components'
 class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { 
+      
+     }
   }
   render() { 
-    return ( 
+    return (
       <StyledHeader>
-        <nav></nav>
+        <a className='title'>Get Service</a>
+        <nav>
+          <ol>
+            <li>
+              <a href=''>Sonic</a>
+            </li>
+            <li>
+              <a href=''>Mazda5</a>
+            </li>
+          </ol>
+        </nav>
+        <div className='menu'>
+          <div className='bars top'/>
+          <div className='bars mid'/>
+          <div className='bars bot'/>
+        </div>
       </StyledHeader>
-     );
+    )
   }
 }
  
 export default Header;
 
 const StyledHeader = styled.header`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  background: black;
+  color: white;
+  .title {
+    font-family: Tesla;
+    font-size: 20px;
+    margin: 20px;
+  }
+  
+  ol {
+    list-style: none;
+    display: flex;
+    justify-content: space-around;
+    font-family: 'Nunito Sans', sans-serif;
+    li a {
+      margin: 0 25px;
+      color: white;
+      text-decoration: none;
+    }
+    a:hover {
+      color: grey;
+      transition: color 300ms;
+    }
+  }
+  .menu {
+    height: 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  .bars {
+    width: 20px;
+    height: 2px;
+    background: white;
 
+  }
 `
